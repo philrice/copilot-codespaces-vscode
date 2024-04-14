f04bcaa8b1580c512adc0f92bb52365c17aaa971
Phil Rice <phil@philrice.uk>
2024-04-14 12:43:00 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,40 +1,26 @@
 // create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// create server
-http.createServer(function(request, response){
-    // get request url
-    var path = url.parse(request.url, true).pathname;
-    if(path === '/'){
-        // read html file
-        fs.readFile('comment.html', 'utf8', function(error, data){
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.end(data);
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if(err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
         });
-    } else if(path === '/comment'){
-        if(request.method === 'POST'){
-            // create variable to save post data
-            var body = '';
-            // get post data
-            request.on('data', function(data){
-                body += data;
-            });
-            // get post data
-            request.on('end', function(){
-                var post = qs.parse(body);
-                // response to client
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.end(JSON.stringify(post));
-            });
-        }
-    } else {
-        response.writeHead(404, {'Content-Type': 'text/html'});
-        response.end('404 Not Found');
     }
-}).listen(52273, function(){
-    console.log('Server Running at http:// localhost:52273');
-}
-);
+
+    if(req.url === '/api/comments') {
+        fs.readFile(path.join(__dirname, 'public', 'comments.json'), (err, content) => {
+            if(err) throw err;
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(content);
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
